59a0a1ca79ddf5828d2467293ce4080d2e15ae91
Rodrigo Caneppele <r.caneppele@gmail.com>
2020-11-16 17:09:22 -0300

    codigos da aula 1

---
+++ src/test/java/br/com/alura/leilao/HelloWorldSelenium.java
@@ -0,0 +1,16 @@
+package br.com.alura.leilao;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HelloWorldSelenium {
+
+	@Test
+	public void helloTest() {
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+		WebDriver browser = new ChromeDriver();
+		browser.navigate().to("http://localhost:8080/leiloes");
+	}
+
+}
