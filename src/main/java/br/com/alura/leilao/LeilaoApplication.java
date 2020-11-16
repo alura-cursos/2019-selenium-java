4bc1198303fb2bcce5a6371fce650679413c0616
Rodrigo Caneppele <r.caneppele@gmail.com>
2020-11-16 15:50:49 -0300

    projeto inicial

---
+++ src/main/java/br/com/alura/leilao/LeilaoApplication.java
@@ -0,0 +1,14 @@
+package br.com.alura.leilao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeilaoApplication {
+
+	// -Dspring.profiles.active=test
+	public static void main(String[] args) {
+		SpringApplication.run(LeilaoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/alura/leilao/service/LanceService.java
@@ -0,0 +1,45 @@
+package br.com.alura.leilao.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.alura.leilao.dto.NovoLanceDto;
+import br.com.alura.leilao.model.Lance;
+import br.com.alura.leilao.model.Leilao;
+import br.com.alura.leilao.model.Usuario;
+import br.com.alura.leilao.repositories.LanceRepository;
+import br.com.alura.leilao.repositories.LeilaoRepository;
+import br.com.alura.leilao.repositories.UsuarioRepository;
+
+@Service
+public class LanceService {
+
+	@Autowired
+	private LanceRepository lances;
+
+	@Autowired
+	private UsuarioRepository usuarios;
+
+	@Autowired
+	private LeilaoRepository leiloes;
+
+	public boolean propoeLance(NovoLanceDto lanceDto, String nomeUsuario) {
+
+		Usuario usuario = usuarios.getUserByUsername(nomeUsuario);
+		Lance lance = lanceDto.toLance(usuario);
+
+		Leilao leilao = this.getLeilao(lanceDto.getLeilaoId());
+
+		if (leilao.propoe(lance)) {
+			lances.save(lance);
+			return true;
+		}
+
+		return false;
+	}
+
+	public Leilao getLeilao(Long leilaoId) {
+		return leiloes.getOne(leilaoId);
+	}
+
+}
